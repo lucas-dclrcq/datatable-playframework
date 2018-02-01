00792a6e2baecf192d4d2215701c07b3424c0848
Lucas Declercq <lucas.declercq@ineat-conseil.fr>
2018-02-01 11:44:51 +0100

    feat(controller): add constants for datatable query parameters

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -6,6 +6,13 @@ import views.html.*;
 
 
 public class HomeController extends Controller {
+    private static final String DATATABLE_PARAM_DRAW = "draw";
+    private static final String DATATABLE_PARAM_SEARCH = "search[value]";
+    private static final String DATATABLE_PARAM_DISPLAY_LENGTH = "length";
+    private static final String DATATABLE_PARAM_DISPLAY_START = "start";
+    private static final String DATATABLE_PARAM_SORTING_COLUMN = "order[0][column]";
+    private static final String DATATABLE_PARAM_SORTING_ORDER = "order[0][dir]";
+
     public Result index() {
         return ok(index.render("DataTable with Play! Framework"));
     }
