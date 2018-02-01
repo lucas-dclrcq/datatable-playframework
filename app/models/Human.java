9a981afb29be8a55931efe18e7d356975386e238
Lucas Declercq <lucas.declercq@ineat-conseil.fr>
2018-02-01 11:43:16 +0100

    feat(model): add Human model

---
+++ app/models/Human.java
@@ -0,0 +1,13 @@
+package models;
+
+/**
+ * Created by Lucas Declercq on 2/1/18
+ */
+public class Human {
+    public Long id;
+    public String firstName;
+    public String lastName;
+    public String country;
+    public String hairColor;
+    public String occupation;
+}
